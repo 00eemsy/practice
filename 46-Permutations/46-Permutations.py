cfb8357a1d788035a098144380f33d621b795d53
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2026-01-06 15:36:23 -0800

    File updated at 1/6/2026, 3:36:23 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,21 +1,21 @@
-# Last updated: 1/6/2026, 3:31:28 PM
+# Last updated: 1/6/2026, 3:36:23 PM
 1class Solution:
 2    def permute(self, nums: List[int]) -> List[List[int]]:
 3        ret = []
-4
-5        def helper(curr, i):
-6            # print('curr: ' + str(curr) + ', i: ' + str(i))
-7            if len(curr) == len(nums):
-8                ret.append(curr[::])
-9                return
-10
-11            temp = set(curr)
-12
-13            for j in range(len(nums)):
-14                if j != i and nums[j] not in temp:
-15                    curr.append(nums[j])
-16                    helper(curr, j)
-17                    curr.pop()
+4        length = len(nums)
+5
+6        def helper(curr, n):
+7            # print('curr: ' + str(curr) + ', n: ' + str(n))
+8            if len(curr) == length:
+9                ret.append(curr[::])
+10                return
+11
+12            for j in range(len(n)):
+13                curr.append(nums[j])
+14                temp = n.pop(j)
+15                helper(curr, n)
+16                curr.pop()
+17                n.insert(j, temp)
 18
-19        helper([], -1)
+19        helper([], nums)
 20        return ret
