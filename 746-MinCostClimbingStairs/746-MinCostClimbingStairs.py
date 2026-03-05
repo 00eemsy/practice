5f20aa33933bb79a06a908cfab2274108f3850ce
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2026-03-05 12:17:41 -0800

    File updated at 3/5/2026, 12:17:40 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -1,9 +1,12 @@
-# Last updated: 1/2/2026, 3:38:45 PM
+# Last updated: 3/5/2026, 12:17:40 PM
 1class Solution:
 2    def minCostClimbingStairs(self, cost: List[int]) -> int:
-3        d = {0:cost[0], 1:cost[1]}
+3        d = {len(cost)-1: cost[-1], len(cost)-2: cost[len(cost)-2]}
 4
-5        for i in range(2,len(cost)):
-6            d[i] = min(d[i-1], d[i-2]) + cost[i]
-7
-8        return min(d[len(cost) - 1], d[len(cost) - 2])
+5        i = len(cost)-3
+6
+7        while i >= 0:
+8            d[i] = cost[i] + min(d[i+1], d[i+2])
+9            i -= 1
+10
+11        return min(d[0], d[1])
